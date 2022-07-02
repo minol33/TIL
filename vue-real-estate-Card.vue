600719e96c0ce9a3ad0a5a1f3fbba81b1a94cffb
minol33 <88490416+minol33@users.noreply.github.com>
2022-07-02 16:44:31 +0900

    Create vue-real-estate-Card.vue
    card.vue code

---
+++ vue-real-estate-Card.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <img :src="oneroom.image" class="room-img">
+        <h4 @click="$emit('openModal', oneroom.id)">{{oneroom.title}}</h4>
+        <!-- <h4 @click="modalOpen = true; roomNum = i">{{a.title}}</h4> -->
+        <p>{{oneroom.content}}</p>
+        <p>{{oneroom.price}} 원</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'Card',
+    props : {
+        oneroom : Object,
+    }
+}
+</script>
+
+<style>
+
+</style>
